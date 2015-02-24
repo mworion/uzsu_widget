3bd33bd9be2c63920f18c611330ed103afa4b7f6
mworion <michael.wuertenberger@t-online.de>
2015-02-24 13:06:06 +0100

    v 1.71 hotfix zur Zeiteingabe

--- visu.js
+++ visu.js
@@ -1,7 +1,7 @@
 // 
 // Neugestaltetes UZSU Widget zur Bedienung UZSU Plugin
 //
-// Release 1.7
+// Release 1.71
 //
 // Darstellung der UZSU Einträge und Darstellung Widget in Form eine Liste mit den Einträgen
 // Umsetzung
@@ -143,7 +143,7 @@ function uzsuBuildTableRow(numberOfRow, designType, valueType, textSelectList){
 					template += "<\/select><\/div><\/form><\/td>";
 				}
 				// time
-				template += "<td><input type='time' data-clear-btn='true' style = 'width:350px' id='uzsuEntryTime" + numberOfRow +"'>";
+				template += "<td><input type='text' data-clear-btn='true' style = 'width:350px' id='uzsuEntryTime" + numberOfRow +"'>";
 				// rrule
 				// hier wird nur der textstring übernommen. prüfungen erfolgen keine !
 				template += "<input type='text' data-clear-btn='true' style = 'width:350px' id='uzsuEntryRrule" + numberOfRow +"'><\/td>";
@@ -180,7 +180,7 @@ function uzsuBuildTableRow(numberOfRow, designType, valueType, textSelectList){
 				template += "<\/select><\/div><\/form><\/td>";
 			}
 			// time
-			template += "<td><input type='time' data-clear-btn='false' id='uzsuEntryTime" + numberOfRow +"'>";
+			template += "<td><input type='text' data-clear-btn='false' id='uzsuEntryTime" + numberOfRow +"'>";
 			// rrule
 			template += "<form><fieldset data-role='controlgroup' data-type='horizontal' data-mini='true'>";
 			for(numberOfDay = 0; numberOfDay < 7; numberOfDay ++){
@@ -219,7 +219,7 @@ function uzsuBuildTableFooter(designType){
 			template += "<div data-role = 'button' id = 'uzsuSortTime'> Sort Times<\/div>";
 		}
 		template += "<div data-role = 'button' id = 'uzsuCancel'> Cancel <\/div> <\/td>";
-	template += "<td style = 'text-align: right'><h6> v1.7 <\/h6><\/td><\/div><\/tr><\/table>";
+	template += "<td style = 'text-align: right'><h6> v1.71 <\/h6><\/td><\/div><\/tr><\/table>";
 	// abschlus des gesamten span container
 	template += "<\/span>";
     // und der abschluss des popup divs
